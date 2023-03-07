66b1e0b637de3119e6a3e93bc23f8e952d58b7fa
Jordan Kennedy <jkennedy@simpleviewinc.com>
2023-03-07 10:13:57 -0700

    GROK-1880: Create a client library and add a prefix to be used by graphServer • Converted private TrainingPrefix.js variables to public variables. • Removed wrapper methods

--- src/prefixes/TrainingPrefix.js
+++ src/prefixes/TrainingPrefix.js
@@ -7,39 +7,15 @@ class TrainingPrefix {
         this._graphUrl = graphUrl;
         this._graphServer = graphServer;
 
-        this._movies = new MoviesPrefix({
+        this.movies = new MoviesPrefix({
             graphUrl,
             graphServer
         });
 
-        this._people = new PeoplePrefix({
+        this.people = new PeoplePrefix({
             graphUrl,
             graphServer
         });
-    }
-
-    async movies(...args) {
-        return this._movies.find(...args);
-    }
-
-    async movies_insert(...args) {
-        return this._movies.insert(...args);
-    }
-
-    async movies_remove(...args) {
-        return this._movies.remove(...args);
-    }
-
-    async people(...args) {
-        return this._people.find(...args);
-    }
-
-    async people_insert(...args) {
-        return this._people.insert(...args);
-    }
-
-    async people_remove(...args) {
-        return this._people.remove(...args);
     }
 }
 
