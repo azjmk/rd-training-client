2bf20e236990fe85cd6d80610ffc7c82fbf114c9
Jordan Kennedy <jkennedy@simpleviewinc.com>
2023-03-20 17:34:49 -0700

    GROK-1883: Graphql Unit Tests

--- src/prefixes/TrainingPrefix.js
+++ src/prefixes/TrainingPrefix.js
@@ -1,5 +1,6 @@
+const { query } = require("@simpleview/sv-graphql-client");
 const MoviesPrefix = require("./collections/MoviesPrefix");
-const PeoplePrefix = require("./collections/PeoplePrefix")
+const PeoplePrefix = require("./collections/PeoplePrefix");
 
 class TrainingPrefix {
 	constructor({ graphUrl, graphServer }) {
@@ -16,6 +17,46 @@ class TrainingPrefix {
 			graphUrl,
 			graphServer
 		});
+	}
+
+	async clearTestData({ fields = `success message`, context, filter, headers } = {}) {
+		const response = await query({
+			query: `
+				mutation {
+					training {
+						training_test_clear_data {
+							${fields}
+						}
+					}
+				}
+			`,
+			url: this._graphUrl,
+			headers,
+			key: "training.training_test_clear_data",
+			clean: true
+		});
+
+		return response;
+	}
+
+	async resetTestData({ fields = `success message`, context, input, headers } = {}) {
+		const response = await query({
+			query: `
+				mutation {
+					training {
+						training_test_reset_data {
+							${fields}
+						}
+					}
+				}
+			`,
+			url: this._graphUrl,
+			headers,
+			key: "training.training_test_reset_data",
+			clean: true
+		});
+
+		return response;
 	}
 }
 
